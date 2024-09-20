7bb02e3687fd5ed2f56d29ae464f8f6a89460cef
Janemwaura <mwaura461@gmail.com>
2024-09-20 14:31:21 +0300

    styled hero.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
-import Header from './Header'
-import './Hero.css'
+import React from 'react';
+import Header from './Header';
+import './Hero.css';
+import hero_image from '/home/janewmwaura/personalprojectportfolio/FitClub-Starter/src/assets/hero_image.png';
+import hero_image_back from '/home/janewmwaura/personalprojectportfolio/FitClub-Starter/src/assets/hero_image_back.png';
+import Heart from '/home/janewmwaura/personalprojectportfolio/FitClub-Starter/src/assets/heart.png';
+import Calories from '/home/janewmwaura/personalprojectportfolio/FitClub-Starter/src/assets/calories.png';
+
 const Hero = () => {
     return (
         <div className="hero">
@@ -12,8 +17,66 @@ const Hero = () => {
                     <div></div>
                     <span>the best fitness club in the town</span>
                 </div>
+                {/*hero headings*/}
+                <div className="hero-text">
+                    <div>
+                        <span className='stroke-text'>Shape</span> 
+                        <span>Your</span>
+
+                    </div>
+                    <div>
+                        <span>Ideal body</span>
+                    </div>
+                    <div>
+                        <span>
+                        In here we will help you to shape and build your ideal body and 
+                        live up your life to fullest
+                        </span>
+                    </div>
                 </div>
-            <div className="right-h">right side</div>  
+                
+
+                <div className="figures">
+                    <div>
+                        <span>+140</span>
+                        <span>EXPERT COACHES</span>
+                        </div>
+                    <div>
+                        <span>+978</span>
+                        <span>MEMBERS JOINED</span>
+                        </div>
+                    <div>
+                        <span>+50</span>
+                        <span>FITNESS PROGRAMS</span>
+                        </div>
+                </div>
+                {/*hero buttons*/}
+                <div className="hero-buttons">
+                    <buttons className="btn">Get Started</buttons>
+                    <buttons className="btn">Learn More</buttons>
+                </div>
+                </div>
+            <div className="right-h">
+                <button className="btn">Join Now</button>
+                <div className="heart-rate">
+                    <img src={Heart} alt=""/>
+                    <span>Heart Rate</span>
+                    <span>116 bpm</span>
+                </div>
+
+            {/*hero images*/} 
+            <img src={hero_image} alt="" className="hero-image" />
+            <img src={hero_image_back} alt="" className="hero-image_back" />
+            {/*calories*/} 
+            <div className="calories">
+                <img src={Calories} alt=""/>
+                <div>
+                <span>Calories Burned</span>
+                <span>220 kcal</span>
+                </div>
+            </div>
+
+                </div>  
         </div>
     )
 }
